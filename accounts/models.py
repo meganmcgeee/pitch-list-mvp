ab97b7fefe4f62acf42194e18812d4b66de993c0
meganmcgeee <meganpmcgee@gmail.com>
2018-05-21 11:57:53 -0700

    User profile now exists as part of admin panel. Need to update user auth handling. Renamed simplesocial to pitchlist.

--- accounts/models.py
+++ accounts/models.py
@@ -3,30 +3,36 @@ from django.contrib import auth
 from django.db.models.signals import post_save #for updating and saving user profiles
 from django.dispatch import receiver
 
+
 # Create your models here.
 class User(auth.models.User, auth.models.PermissionsMixin):
 
     def __str__(self):
         return "@{}".format(self.username) 
 
-class Profile(models.Model):
+class UserProfileModel(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE),
     MEDIUMS_CHOICES = (
         ('Print', 'Print'),
         ('Digital', 'Digital'),
         ('Radio', 'Radio'),
         ('Vlog', 'Vlog'),
     )
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
     career = models.CharField(max_length=250, blank=True)
     bio = models.TextField(blank=True)
     location = models.CharField(max_length=250, blank=True)
     clients = models.TextField(blank=True)
 
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.created(user=instance)
+    class Meta:
+        verbose_name = "Profile"
+        verbose_name_plural = "Profiles"
 
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
+# Save, and Create
+# @receiver(post_save, sender=User)
+# def create_user_profile(sender, instance, created, **kwargs):
+#     if created:
+#         UserProfileModel.objects.created(user=instance)
+
+# @receiver(post_save, sender=User)
+# def save_user_profile(sender, instance, **kwargs):
+#     instance.profile.save()
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic import CreateView
+from django.contrib.auth.models import User
+from accounts.models import UserProfileModel
 from . import forms
 
 # Create your views here.
@@ -8,3 +10,4 @@ class SignUp(CreateView):
     form_class = forms.UserCreateForm
     success_url = reverse_lazy('login')
     template_name = 'accounts/signup.html'
+    # UserProfileModel.objects.create(user=user)
